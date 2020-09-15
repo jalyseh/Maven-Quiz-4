dfb3093728f64634845ff8475f68d59dd4de9aaf
jalyseh <hammettjadyn@gmail.com>
2020-09-15 11:42:55 -0400

    First commit

--- src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
+++ src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
@@ -5,18 +5,53 @@ package rocks.zipcode.quiz4.arrays;
  */
 public class ArrayUtils {
     public static String getMiddleElement(String[] values) {
-        return null;
+
+
+        return values[values.length / 2];
+
     }
 
     public static String[] removeMiddleElement(String[] values) {
-        return null;
+
+        String[] remove = new String[values.length-1];
+
+        for(int index = 0; index < values.length; index++) {
+            if(index == values.length / 2){
+
+            }
+            else if (index > values.length/2){
+                remove[index-1] = values[index];
+            }
+            else {
+                remove[index] = values[index];
+
+            }
+        }
+
+        return remove;
     }
 
     public static String getLastElement(String[] values) {
-        return null;
+
+        for(int index = 0; index < values.length; index ++){
+
+        }
+        return values[values.length -1];
     }
 
     public static String[] removeLastElement(String[] values) {
-        return null;
+
+        String[] remove = new String[values.length-1];
+
+        for(int index = 0; index < values.length-1; index++) {
+            if (index == values.length - 1) {
+
+            } else {
+                remove[index] = values[index];
+
+            }
+        }
+        return remove;
+
     }
 }
--- src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
@@ -1,45 +1,114 @@
 package rocks.zipcode.quiz4.fundamentals;
 
+
 /**
  * @author leon on 21/12/2018.
  */
-public class StringUtils {
+public class StringUtils<answer> {
     public static Character getMiddleCharacter(String string) {
 
         char atIndex = string.charAt(string.length() / 2);
         return atIndex;
     }
 
-    public static String capitalizeMiddleCharacter(String str) {
+    public static String capitalizeMiddleCharacter(String str)
+    {
 
-        String Index = str.length();
+        String answer = "";
+        char middleChar = str.charAt(str.length() / 2);
 
-
-        return null;
+         for(int x = 0; x < str.length(); x++)
+         {
+             if(x != str.length() / 2)
+             {
+                 answer += str.charAt(x);
+             }
+             else
+                 { answer += String.valueOf(middleChar).toUpperCase();
+                 }
+             }
+        return answer;
     }
-
     public static String lowerCaseMiddleCharacter(String str) {
 
-        return null;
+        String answer = "";
+        char middleChar = str.charAt(str.length() / 2);
+
+        for(int x = 0; x < str.length(); x++)
+        {
+            if(x != str.length() / 2)
+            {
+                answer += str.charAt                                                                                                                             (x);
+            }
+            else
+            { answer += String.valueOf(middleChar).toLowerCase();
+            }
+        }
+        return answer;
+
     }
 
     public static Boolean isIsogram(String str) {
 
-        return null;
+        Boolean answer = true;
+        for (int index = 0; index < str.length(); index++)
+        {
+            for(int index2 = index + 1; index2 < str.length(); index2++)
+            {
+                if(String.valueOf(str.charAt(index)).equals(String.valueOf(str.charAt(index2)))) {
+                    answer = false;
+                }
+            }
+        }
+        return answer;
     }
 
     public static Boolean hasDuplicateConsecutiveCharacters(String str) {
 
-        return null;
+        Boolean consec = false;
+        for(int index = 0; index < str.length()-1; index++){
+            if(str.charAt(index) == str.charAt(index + 1)){
+                consec = true;
+            }
+        }
+
+        return consec;
     }
 
     public static String removeConsecutiveDuplicateCharacters(String str) {
 
-        return null;
+        for(int index = 0; index < str.length(); index++) {
+            char char1 = str.charAt(index);
+            char char2 = ' ';
+
+            if(index + 1 < str.length()) {
+            char2 = str.charAt(index + 1);
+
+            }
+            if(char1 == char2){
+                String stringVal = String.valueOf(char1);
+                str = str.replace(stringVal, "");
+            }
+        }
+        return str;
     }
 
     public static String invertCasing(String str) {
 
-        return null;
+        char[] chars = str.toCharArray();
+
+        for(int index = 0; index < str.length(); index++){
+            char char1 = str.charAt(index);
+
+            if (Character.isLowerCase(char1)){
+                String newChar = String.valueOf(Character.toUpperCase(char1));
+                chars[index] = newChar.charAt(0);
+            }
+            else if (Character.isUpperCase(char1)){
+                String newChar = String.valueOf(Character.toLowerCase(char1));
+                chars[index] = newChar.charAt(0);
+            }
+        }
+        return new String(chars);
     }
 }
